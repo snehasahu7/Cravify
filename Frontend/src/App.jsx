d996509fed1cb41a9d5092790db06dcb39c6e6fe
snehasahu7 <btech15146.22@bitmesra.ac.in>
2025-02-13 18:41:43 +0530

    Init

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Placeorder from './Pages/PlaceOrder/Placeorder'
+import Cart from './Pages/Cart/Cart'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+         < Route path='/' element={<Home/>}/>
+         < Route path='/cart' element={<Cart/>}/>
+         < Route path='/order' element={<Placeorder/>}/>
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
